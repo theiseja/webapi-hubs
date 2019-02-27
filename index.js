722081e1060d4e5152623f48b4c862e9fe674081
Jesse Theisen <theiseja@gmail.com>
2019-02-26 21:24:41 -0600

    MVP: GET request for /now made

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const express = require('express');
 const server = express();
 const PORT = '9090';
 
+
+server.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+server.get('/now', (req, res) => {
+    const now = new Date().toString(); // sends back the current date & time
+    res.send(now);
+});
+
+
+
+// should be last in the codebase
 server.listen(PORT, () => {
     console.log(`Our Server is listenning on port ${PORT}`)
 });
